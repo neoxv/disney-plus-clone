8718164d784b591440f8afcb31e4ee45062d5169
neoxv <yaredoo2k4@gmail.com>
2022-10-08 18:13:01 +0300

    ADD: Homepage

---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Detail() {
+  return (
+    <Container>
+        Detail
+    </Container>
+  )
+}
+
+export default Detail
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImageSlider from './ImageSlider'
+import Movies from './Movies'
 import Viewers from './Viewers'
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <Container>
       <ImageSlider/>
       <Viewers/>
+      <Movies />
     </Container>
   )
 }
@@ -19,6 +21,7 @@ const Container = styled.main`
   padding: 0 calc(3.5vw + 5px);
   position: relative;
   overflow-x: hidden;
+  // overflow-y: hidden;
 
   &:before{
     background: url("/images/home-background.png") center center / cover no-repeat fixed;
